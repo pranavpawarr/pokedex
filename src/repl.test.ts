c74cc2b17982afd45f998f883bb527a94c4fba3d
pranavpawarr <pranavpawar503@gmail.com>
2025-10-25 02:58:51 +0530

    first commit

---
+++ src/repl.test.ts
@@ -0,0 +1,34 @@
+import { cleanInput } from "./repl";
+import { describe, expect, test } from "vitest";
+
+describe.each([
+  {
+    input: "  hello  world  ",
+    expected: ["hello", "world"],
+  },
+  {
+    input: "Charmander Bulbasaur PIKACHU",
+    expected: ["charmander", "bulbasaur", "pikachu"],
+  },
+  {
+    input: "  SQUIRTLE   ",
+    expected: ["squirtle"],
+  },
+  {
+    input: "multiple    spaces    between",
+    expected: ["multiple", "spaces", "between"],
+  },
+  {
+    input: "single",
+    expected: ["single"],
+  },
+])("cleanInput($input)", ({ input, expected }) => {
+  test(`Expected: ${expected}`, () => {
+    const actual = cleanInput(input);
+
+    expect(actual).toHaveLength(expected.length);
+    for (const i in expected) {
+      expect(actual[i]).toBe(expected[i]);
+    }
+  });
+});
