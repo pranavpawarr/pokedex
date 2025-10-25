cfeb84bed79f1a1182e0ae50e78d38dd4e5d691d
pranavpawarr <pranavpawar503@gmail.com>
2025-10-26 01:05:14 +0530

    refactoring code

--- src/repl.ts
+++ src/repl.ts
@@ -1,61 +1,38 @@
-import * as readline from "readline";
-import type { CLICommand } from "./command.js";
-import { commandExit } from "./command_exit.js";
-import { commandHelp } from "./command_help.js";
+import type { State } from "./state.js";
 
 export function cleanInput(input: string): string[] {
   return input.trim().toLowerCase().split(/\s+/);
 }
 
-export function getCommands(): Record<string, CLICommand> {
-  return {
-    help: {
-      name: "help",
-      description: "Displays a help message",
-      callback: commandHelp,
-    },
-    exit: {
-      name: "exit",
-      description: "Exit the Pokedex",
-      callback: commandExit,
-    },
-  };
-}
+export function startREPL(state: State): void {
+  state.rl.prompt();
 
-export function startREPL() {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    prompt: "Pokedex > ",
-  });
-
-  rl.prompt();
-
-  rl.on("line", async (input) => {
+  state.rl.on("line", async (input) => {
     const words = cleanInput(input);
+
     if (words.length === 0) {
-      rl.prompt();
+      state.rl.prompt();
       return;
     }
 
     const commandName = words[0];
+    const cmd = state.commands[commandName];
 
-    const commands = getCommands();
-    const cmd = commands[commandName];
     if (!cmd) {
       console.log(
         `Unknown command: "${commandName}". Type "help" for a list of commands.`
       );
-      rl.prompt();
+      state.rl.prompt();
       return;
     }
 
     try {
-      cmd.callback(commands);
+      cmd.callback(state);
     } catch (e) {
       console.log(e);
     }
 
-    rl.prompt();
+    console.log();
+    state.rl.prompt();
   });
 }
---
+++ src/state.ts
@@ -0,0 +1,40 @@
+import { createInterface, type Interface } from "readline";
+import { commandExit } from "./command_exit.js";
+import { commandHelp } from "./command_help.js";
+
+export type CLICommand = {
+  name: string;
+  description: string;
+  callback: (state: State) => void;
+};
+
+export interface State {
+  rl: Interface;
+  commands: Record<string, CLICommand>;
+}
+
+export function initState(): State {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+
+  const commands: Record<string, CLICommand> = {
+    help: {
+      name: "help",
+      description: "Displays a help message",
+      callback: commandHelp,
+    },
+    exit: {
+      name: "exit",
+      description: "Exit the Pokedex",
+      callback: commandExit,
+    },
+  };
+
+  return {
+    rl: rl,
+    commands: commands,
+  };
+}
